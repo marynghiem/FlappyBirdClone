086f7af5a618f7580ec642de00667f8bdf6c4587
marynghiem <marynghiem46@gmail.com>
2022-06-08 12:59:24 -0700

    created the entities

---
+++ entities/index.js
@@ -0,0 +1,12 @@
+import { Matter } from "matter-js";
+export default (restart) => {
+  let engine = Matter.Engine.create({ enableSleeping: false });
+
+  let world = engine.world;
+
+  world.gravity.y = 0.4;
+
+  return {
+    physics: { engine, world },
+  };
+};
