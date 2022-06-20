70db7f09a569ad20292275d338f16566a4645ba3
marynghiem <marynghiem46@gmail.com>
2022-06-20 12:05:55 -0700

    added gravity to flappy bird clone

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import entities from "./entities";
+import Physics from "./physics";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <GameEngine
+        systems={[Physics]}
         entities={entities()}
         style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
       ></GameEngine>
