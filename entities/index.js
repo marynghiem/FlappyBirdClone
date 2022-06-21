10f2469c87ce96271471e0a38463d3d5abda29ce
marynghiem <marynghiem46@gmail.com>
2022-06-21 11:15:03 -0700

    created floor for flappy bird clone

--- entities/index.js
+++ entities/index.js
@@ -1,5 +1,10 @@
 import Matter from "matter-js";
 import Bird from "../components/Bird";
+import Floor from "../components/Floor";
+import { Dimensions } from "react-native";
+
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
 
 export default (restart) => {
   let engine = Matter.Engine.create({ enableSleeping: false });
@@ -10,6 +15,7 @@ export default (restart) => {
 
   return {
     physics: { engine, world },
-    Bird: Bird(world, "green", { x: 50, y: 200 }, { height: 40, width: 40 }),
+    Bird: Bird(world, "green", { x: 50, y: 300 }, { height: 40, width: 40 }),
+    Floor: Floor(world, "green", { x: windowWidth / 2, y: windowHeight }, { height: 50, width: windowWidth }),
   };
 };
