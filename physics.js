30d7c86342e3c1948b0a43fd3aa83d1a2537ff30
marynghiem <marynghiem46@gmail.com>
2022-06-22 12:08:30 -0700

    fixed bug regarding counting issue

--- physics.js
+++ physics.js
@@ -26,6 +26,8 @@ const Physics = (entities, { touches, time, dispatch }) => {
       const pipeSizePos = getPipeSizePosPair(windowWidth * 0.9);
       Matter.Body.setPosition(entities[`ObstacleTop${index}`].body, pipeSizePos.pipeTop.pos);
       Matter.Body.setPosition(entities[`ObstacleBottom${index}`].body, pipeSizePos.pipeBottom.pos);
+
+      entities[`ObstacleTop${index}`].point = false;
     }
     Matter.Body.translate(entities[`ObstacleTop${index}`].body, { x: -3, y: 0 });
     Matter.Body.translate(entities[`ObstacleBottom${index}`].body, { x: -3, y: 0 });
