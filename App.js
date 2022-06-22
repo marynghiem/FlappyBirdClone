b4a1f1bbe491cd613e5ea969fad30a08fa121d07
marynghiem <marynghiem46@gmail.com>
2022-06-21 17:12:35 -0700

    completed game and was able to add the button to restart game

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
 import React, { useState, useEffect } from "react";
 import { GameEngine } from "react-native-game-engine";
 import entities from "./entities";
 import Physics from "./physics";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   const [running, setRunning] = useState(false);
@@ -11,11 +11,12 @@ export default function App() {
   const [currentPoints, setCurrentPoints] = useState(0);
 
   useEffect(() => {
-    setRunning(true);
+    setRunning(false);
   }, []);
 
   return (
     <View style={{ flex: 1 }}>
+      <Text style={{ textAlign: "center", fontSize: 40, fontWeight: "bold", margin: 20 }}>{currentPoints}</Text>
       <GameEngine
         ref={(ref) => {
           setGameEngine(ref);
@@ -28,7 +29,6 @@ export default function App() {
             case "game_over":
               setRunning(false);
               gameEngine.stop();
-              setCurrentPoints(0);
               break;
             case "new_point":
               setCurrentPoints(currentPoints + 1);
@@ -36,8 +36,23 @@ export default function App() {
           }
         }}
         style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
-      ></GameEngine>
-      <StatusBar style="auto" hidden={true} />
+      >
+        <StatusBar style="auto" hidden={true} />
+      </GameEngine>
+      {!running ? (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <TouchableOpacity
+            style={{ backgroundColor: "black", paddingHorizontal: 30, paddingVertical: 10 }}
+            onPress={() => {
+              setCurrentPoints(0);
+              setRunning(true);
+              gameEngine.swap(entities());
+            }}
+          >
+            <Text style={{ fontWeight: "bold", color: "white", fontSize: 30 }}>START GAME</Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
